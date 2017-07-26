8321e3307af8a3b59790f04512b490baaede5bc9
Helmut Hoffer von Ankershoffen <hhva@20steps.de>
2017-07-26 20:04:12 +0200

    remove angular-ui-router require

--- src/javascripts/vendors.js
+++ src/javascripts/vendors.js
@@ -1,15 +1,15 @@
 global.rangy = require('rangy');
 global.rangy = require('rangy/lib/rangy-selectionsaverestore');
 global.numeral = require('numeral');
+global._ = require('lodash');
 
-require('angular');
-require('angular-ui-router');
+//require('angular');
+//require('angular-ui-router');
 require('nginflection');
 require('ui-select');
-require('angular-translate');
+//require('angular-translate');
 
 require('angular-numeraljs');
 require('angular-ui-bootstrap/dist/ui-bootstrap-tpls');
-require('ng-file-upload');
+//require('ng-file-upload');
 
-global._ = require('lodash');
